84389bb0e77c8cdc5dfa51183b7676cb57f557d5
Molkobain <lajarige.guillaume@free.fr>
2022-10-06 21:57:58 +0200

    Add french translations

---
+++ fr.dict.authent-token.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'AuthentToken:CopyToken' => 'Les identifiants que vous aurez à fournir sont :<p>auth_token=%1$s</p>Notez les maintenant, vous ne pourrez plus les afficher ensuite !',
+	'AuthentToken:RebuildToken' => 'Regénérer le jeton',
+	'AuthentToken:RebuildToken+' => 'Le jeton d\'authentification sera regénéré, ATTENTION le précédent sera perdu et ne pourra plus être utilisé',
+
+	'Class:UserToken' => 'Utilisateur basé sur un jeton',
+	'Class:UserToken/Attribute:login' => 'Application distante',
+	'Class:UserToken/Attribute:login+' => 'Chaîne d\'identification de l\'application distante',
+));

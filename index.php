e37e6cdef612540ab81c4d923efe98ee6016ebf4
odain-cbd <56586767+odain-cbd@users.noreply.github.com>
2023-05-09 08:08:04 +0200

    Update index.php
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ use Combodo\iTop\AuthentToken\Helper\TokenAuthHelper;
 
 require_once(APPROOT.'application/startup.inc.php');
 
-$oController = new MyAccountController(__DIR__.'/templates', 'authent-token');
+$oController = new MyAccountController(__DIR__.'/templates', TokenAuthHelper::MODULE_NAME);
 $oController->SetDefaultOperation('MainPage');
 $oController->HandleOperation();

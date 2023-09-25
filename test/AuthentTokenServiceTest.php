0c5d2c8acda594e8d3f90bf03a30314816f8032a
odain <olivier.dain@combodo.com>
2023-09-25 10:55:18 +0200

    N°6763 - mark backward compatibility decryption token test as skipped for PHP >=8.x

--- test/AuthentTokenServiceTest.php
+++ test/AuthentTokenServiceTest.php
@@ -27,7 +27,7 @@ class AuthentTokenServiceTest extends ItopDataTestCase {
 	public function testTokenGeneration()
 	{
 		if (version_compare(PHP_VERSION, '8.0') >= 0) {
-			$this->markTestSkipped("no need to test as previous encryption mode will not work anymore this test will fail. customers in that case must regenerate their tokens...");
+			$this->markTestSkipped("(N°6763) no need to test as previous encryption mode will not work anymore this test will fail. customers in that case must regenerate their tokens...");
 		}
 		$oApplicationToken = $this->CreateUserToken();
 

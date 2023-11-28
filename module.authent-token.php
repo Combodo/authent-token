3c66ac2b3c8d7a8d5cccb377a0e5ee0619883e2d
Molkobain <lajarige.guillaume@free.fr>
2023-11-28 20:04:25 +0100

    :bookmark: Increase module version to 2.1.3

--- module.authent-token.php
+++ module.authent-token.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'authent-token/2.1.2',
+	'authent-token/2.1.3',
 	array(
 		// Identification
 		//

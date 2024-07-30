9d6263374213c41f10e8af7997e0d9d260c6fa18
jf-cbd <jimmy.fayolle@combodo.com>
2024-07-30 11:56:42 +0200

    authent token 2.1.7

--- module.authent-token.php
+++ module.authent-token.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'authent-token/2.1.5',
+	'authent-token/2.1.7',
 	array(
 		// Identification
 		//
